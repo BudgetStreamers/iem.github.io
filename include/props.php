db753b02fc468c0acc17e670eab34237a39be51b
BudgetStreamers <37638331+BudgetStreamers@users.noreply.github.com>
2018-05-24 21:40:00 -0400

    Main
    Main

---
+++ include/props.php
@@ -0,0 +1,12 @@
+<?php 
+// Fake values
+$config = Array(
+	"punjab_route" => "xmpp:openfire-bosh:5222",
+	"bosh_service" => "/bosh/",
+	"xmpp_domain" => "laptop.local",
+	"xmpp_mucservice" => "conference.laptop.local",
+	"nwschatadmin" => "akrherz@localhost",	
+	"live_appname" => "NWSChat Live",
+);
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+require_once "/config/settings.inc.php";
+require_once "/include/myview.php";
+
+$t = new MyView();
+$t->content = <<<EOF
+<h3>Weather.IM</h3>
+
+<p>This website is a testbed of weather dissemination technologies powered
+by the Iowa Environmental Mesonet of Iowa State University.  These products
+and services are provided without warranty.
+
+<ul>
+ <li><a href="/iembot/">IEMBot Monitor</a></li>
+ <li><a href="/live/">Live Client</a>  This is an upstream version of the
+ <a href="https://nwschat.weather.gov/live/">NWSChat Live</a> client. This 
+ client interfaces with the XMPP server hosted at weather.im and open to
+ public usage.</li>
+</ul>
+EOF;
+$t->render('single.phtml');
+?>
